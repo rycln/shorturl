d7c567fb333d349061bd74d33fb9be51c46166a2
Khalikov Ruslan <rycln1@gmail.com>
2025-02-23 00:58:30 +0300

    Added tests for /api/shorten handler

--- internal/app/server/server_test.go
+++ internal/app/server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -158,6 +159,103 @@ func TestHandlerVariables_ReturnURL(t *testing.T) {
 			require.Equal(t, test.want.code, res.StatusCode)
 			if res.StatusCode != http.StatusBadRequest {
 				assert.Equal(t, test.want.location, res.Header.Get("Location"))
+			}
+		})
+	}
+}
+
+func TestServerArgs_ShortenAPI(t *testing.T) {
+	app := fiber.New()
+	storage := storage.NewSimpleMemStorage()
+	config := &config.Cfg{
+		ServerAddr:    config.DefaultServerAddr,
+		ShortBaseAddr: config.DefaultBaseAddr,
+	}
+	sa := NewServerArgs(storage, config)
+	Set(app, sa)
+
+	type want struct {
+		code        int
+		resContains string
+		contentType string
+	}
+	tests := []struct {
+		name   string
+		method string
+		path   string
+		body   []byte
+		want   want
+	}{
+		{
+			name:   "Valid test #1",
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			body:   []byte(`{"url":"https://practicum.yandex.ru/"}`),
+			want: want{
+				code:        http.StatusCreated,
+				resContains: "http://localhost:8080/",
+				contentType: "application/json",
+			},
+		},
+		{
+			name:   "Wrong method #1",
+			method: http.MethodGet,
+			path:   "/api/shorten",
+			body:   []byte(`{"url":"https://practicum.yandex.ru/"}`),
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "application/json",
+			},
+		},
+		{
+			name:   "Wrong URL #1",
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			body:   []byte(`{"url":"practicum"}`),
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "application/json",
+			},
+		},
+		{
+			name:   "Wrong content type #1",
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			body:   []byte(`{"url":"https://practicum.yandex.ru/"}`),
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain",
+			},
+		},
+		{
+			name:   "Wrong JSON #1",
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			body:   []byte(`{"url:"https://practicum.yandex.ru/"}`),
+			want: want{
+				code:        http.StatusBadRequest,
+				contentType: "application/json",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bodyReader := bytes.NewReader(test.body)
+			request := httptest.NewRequest(test.method, test.path, bodyReader)
+			request.Header.Set("Content-Type", test.want.contentType)
+
+			res, err := app.Test(request, -1)
+			if err != nil {
+				panic(err)
+			}
+			defer res.Body.Close()
+
+			require.Equal(t, test.want.code, res.StatusCode)
+			if res.StatusCode != http.StatusBadRequest {
+				resBody, err := io.ReadAll(res.Body)
+				require.NoError(t, err)
+				assert.Contains(t, string(resBody), test.want.resContains)
+				assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
 			}
 		})
 	}
