4bf7dd538652789ff397ea116f303521e865aa79
Khalikov Ruslan <rycln1@gmail.com>
2025-02-25 01:03:41 +0300

    Fixed default file path

--- configs/config.go
+++ configs/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"flag"
-	"strings"
 
 	"github.com/caarlos0/env/v11"
 )
@@ -10,8 +9,7 @@ import (
 const (
 	DefaultServerAddr      = ":8080"
 	DefaultBaseAddr        = "http://localhost:8080"
-	DefaultFileStoragePath = "."
-	storageFileName        = "urls"
+	DefaultStorageFileName = "urls"
 )
 
 type Cfg struct {
@@ -25,7 +23,7 @@ func NewCfg() *Cfg {
 
 	flag.StringVar(&cfg.ServerAddr, "a", DefaultServerAddr, "Address and port to start the server (environment variable SERVER_ADDRESS has higher priority)")
 	flag.StringVar(&cfg.ShortBaseAddr, "b", DefaultBaseAddr, "Base address and port for short URL (environment variable BASE_URL has higher priority)")
-	flag.StringVar(&cfg.StorageFilePath, "f", DefaultFileStoragePath, "URL storage file path (environment variable FILE_STORAGE_PATH has higher priority)")
+	flag.StringVar(&cfg.StorageFilePath, "f", DefaultStorageFileName, "URL storage file path (environment variable FILE_STORAGE_PATH has higher priority)")
 	flag.Parse()
 
 	err := env.Parse(cfg)
@@ -45,8 +43,5 @@ func (cfg *Cfg) GetBaseAddr() string {
 }
 
 func (cfg *Cfg) GetFilePath() string {
-	if strings.HasSuffix(cfg.StorageFilePath, "/") {
-		return cfg.StorageFilePath + storageFileName
-	}
-	return cfg.StorageFilePath + "/" + storageFileName
+	return cfg.StorageFilePath
 }
