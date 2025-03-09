50cc261bc1b5f1de8d187d048814840c69cacfac
Khalikov Ruslan <rycln1@gmail.com>
2025-03-09 19:17:52 +0300

    Updated tests for simple storage

--- internal/app/storage/simplestorage_test.go
+++ internal/app/storage/simplestorage_test.go
@@ -7,22 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type testStorager interface {
-	AddURL(context.Context, ShortenedURL) error
-	GetOrigURL(context.Context, string) (string, error)
-}
-
-type testStorage struct {
-	ts testStorager
-}
-
-func NewTestStorage(ts testStorager) testStorage {
-	return testStorage{
-		ts: ts,
-	}
-}
-
-func TestAddURLAndGetURL(t *testing.T) {
+func TestAddURL(t *testing.T) {
 	type want struct {
 		mustContain map[string]string
 		wantErr     bool
@@ -93,8 +78,127 @@ func TestAddURLAndGetURL(t *testing.T) {
 				"https://practicum.yandex.ru/",
 			},
 			want: want{
+				wantErr: true,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			strg := NewSimpleStorage()
+			if assert.Equal(t, len(test.shortURLs), len(test.origURLs), "wrong tests") {
+				var err error
+				for i := range test.shortURLs {
+					surl := NewShortenedURL(test.shortURLs[i], test.origURLs[i])
+					err = strg.AddURL(context.Background(), surl)
+				}
+				if test.want.wantErr {
+					assert.Error(t, err)
+				}
+				for k, v := range test.want.mustContain {
+					origURL := strg.storage[k]
+					if !test.want.wantErr {
+						assert.Equal(t, v, origURL)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestAddBatchURL(t *testing.T) {
+	type want struct {
+		mustContain map[string]string
+		wantErr     bool
+	}
+
+	tests := []struct {
+		name      string
+		shortURLs []string
+		origURLs  []string
+		want      want
+	}{
+		{
+			name: "Several pairs of data #1",
+			shortURLs: []string{
+				"abcdefg",
+				"1234ABC",
+			},
+			origURLs: []string{
+				"https://practicum.yandex.ru/",
+				"https://ya.ru/",
+			},
+			want: want{
 				mustContain: map[string]string{
 					"abcdefg": "https://practicum.yandex.ru/",
+					"1234ABC": "https://ya.ru/",
+				},
+				wantErr: false,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			strg := NewSimpleStorage()
+			if assert.Equal(t, len(test.shortURLs), len(test.origURLs), "wrong tests") {
+				var surls = make([]ShortenedURL, len(test.shortURLs))
+				var err error
+				for i := range test.shortURLs {
+					surl := NewShortenedURL(test.shortURLs[i], test.origURLs[i])
+					surls[i] = surl
+				}
+				err = strg.AddBatchURL(context.Background(), surls)
+				if test.want.wantErr {
+					assert.Error(t, err)
+				}
+				for k, v := range test.want.mustContain {
+					origURL := strg.storage[k]
+					if !test.want.wantErr {
+						assert.Equal(t, v, origURL)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestGetOrigURL(t *testing.T) {
+	type want struct {
+		mustContain map[string]string
+		wantErr     bool
+	}
+
+	tests := []struct {
+		name      string
+		shortURLs []string
+		origURLs  []string
+		want      want
+	}{
+		{
+			name: "Simple test #1",
+			shortURLs: []string{
+				"abcdefg",
+			},
+			origURLs: []string{
+				"https://practicum.yandex.ru/",
+			},
+			want: want{
+				mustContain: map[string]string{
+					"abcdefg": "https://practicum.yandex.ru/",
+				},
+				wantErr: false,
+			},
+		},
+		{
+			name: "Simple test #2",
+			shortURLs: []string{
+				"1234ABC",
+			},
+			origURLs: []string{
+				"https://ya.ru/",
+			},
+			want: want{
+				mustContain: map[string]string{
+					"1234ABC": "https://ya.ru/",
 				},
 				wantErr: false,
 			},
@@ -108,9 +212,6 @@ func TestAddURLAndGetURL(t *testing.T) {
 				"https://practicum.yandex.ru/",
 			},
 			want: want{
-				mustContain: map[string]string{
-					"1234ABC": "https://ya.ru/",
-				},
 				wantErr: true,
 			},
 		},
@@ -118,22 +219,99 @@ func TestAddURLAndGetURL(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			strg := NewSimpleStorage()
-			ts := NewTestStorage(strg)
 
 			if assert.Equal(t, len(test.shortURLs), len(test.origURLs), "wrong tests") {
 				for i := range test.shortURLs {
-					surl := NewShortenedURL(test.shortURLs[i], test.origURLs[i])
-					ts.ts.AddURL(context.Background(), surl)
+					strg.storage[test.shortURLs[i]] = test.origURLs[i]
 				}
 				for k, v := range test.want.mustContain {
-					recOrigURL, err := ts.ts.GetOrigURL(context.Background(), k)
+					origURL, err := strg.GetOrigURL(context.Background(), k)
 					if err != nil {
-						if !test.want.wantErr {
+						if test.want.wantErr {
 							assert.Error(t, err)
 						}
 					}
 					if !test.want.wantErr {
-						assert.Equal(t, v, recOrigURL)
+						assert.Equal(t, v, origURL)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestShortOrigURL(t *testing.T) {
+	type want struct {
+		mustContain map[string]string
+		wantErr     bool
+	}
+
+	tests := []struct {
+		name      string
+		shortURLs []string
+		origURLs  []string
+		want      want
+	}{
+		{
+			name: "Simple test #1",
+			shortURLs: []string{
+				"abcdefg",
+			},
+			origURLs: []string{
+				"https://practicum.yandex.ru/",
+			},
+			want: want{
+				mustContain: map[string]string{
+					"abcdefg": "https://practicum.yandex.ru/",
+				},
+				wantErr: false,
+			},
+		},
+		{
+			name: "Simple test #2",
+			shortURLs: []string{
+				"1234ABC",
+			},
+			origURLs: []string{
+				"https://ya.ru/",
+			},
+			want: want{
+				mustContain: map[string]string{
+					"1234ABC": "https://ya.ru/",
+				},
+				wantErr: false,
+			},
+		},
+		{
+			name: "Wrong GetURL request #1",
+			shortURLs: []string{
+				"abcdefg",
+			},
+			origURLs: []string{
+				"https://practicum.yandex.ru/",
+			},
+			want: want{
+				wantErr: true,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			strg := NewSimpleStorage()
+
+			if assert.Equal(t, len(test.shortURLs), len(test.origURLs), "wrong tests") {
+				for i := range test.shortURLs {
+					strg.storage[test.shortURLs[i]] = test.origURLs[i]
+				}
+				for k, v := range test.want.mustContain {
+					shortURL, err := strg.GetShortURL(context.Background(), v)
+					if err != nil {
+						if test.want.wantErr {
+							assert.Error(t, err)
+						}
+					}
+					if !test.want.wantErr {
+						assert.Equal(t, k, shortURL)
 					}
 				}
 			}
