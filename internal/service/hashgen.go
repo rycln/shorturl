62afa48ee241937b2738419d8998dac5b9431147
Khalikov Ruslan <rycln1@gmail.com>
2025-05-03 18:57:37 +0300

    refactor: added hash generator service

---
+++ internal/service/hashgen.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"crypto/md5"
+
+	"github.com/jxskiss/base62"
+	"github.com/rycln/shorturl/internal/models"
+)
+
+type HashGen struct {
+	len int
+}
+
+func NewHash(len int) *HashGen {
+	return &HashGen{
+		len: len,
+	}
+}
+
+func (s *HashGen) GenerateHashFromURL(orig models.OrigURL) models.ShortURL {
+	hash := md5.Sum([]byte(orig))
+	encodedHash := base62.Encode([]byte(hash[:]))
+	return models.ShortURL(encodedHash[:s.len])
+}
