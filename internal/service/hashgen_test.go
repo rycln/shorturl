92d264f5fd337bfca4d81a4fd9f069fbe2526cd4
Khalikov Ruslan <rycln1@gmail.com>
2025-05-10 02:03:03 +0300

    test: added tests for hash gen service

---
+++ internal/service/hashgen_test.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testHashLen = 10
+
+func TestHashGen_GenerateHashFromURL(t *testing.T) {
+	s := NewHash(testHashLen)
+
+	t.Run("valid test", func(t *testing.T) {
+		hash := s.GenerateHashFromURL(testOrigURL)
+		assert.Len(t, hash, testHashLen)
+	})
+}
