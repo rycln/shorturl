bbca9b28690fd27a4649c79fdb80ecf9a1f746a4
Khalikov Ruslan <rycln1@gmail.com>
2025-05-11 15:32:31 +0300

    refactor: added app layer

--- go.mod
+++ go.mod
@@ -1,44 +1,35 @@
 module github.com/rycln/shorturl
 
-go 1.22.12
+go 1.23.0
 
 toolchain go1.23.3
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/caarlos0/env/v11 v11.3.1
-	github.com/gofiber/contrib/fiberzap/v2 v2.1.5
-	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438
-	github.com/jackc/pgx/v5 v5.7.2
+	github.com/jackc/pgx/v5 v5.7.4
 	github.com/jxskiss/base62 v1.1.0
+	github.com/pressly/goose/v3 v3.24.3
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.6.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.56.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/app/app.go
@@ -0,0 +1,86 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rycln/shorturl/internal/config"
+	"github.com/rycln/shorturl/internal/handlers"
+	"github.com/rycln/shorturl/internal/logger"
+	"github.com/rycln/shorturl/internal/services"
+	"github.com/rycln/shorturl/internal/storage"
+)
+
+const (
+	lengthOfShortURL = 7
+)
+
+type App struct {
+	router  *chi.Mux
+	storage storage.Storage
+	cfg     *config.Cfg
+}
+
+func New() (*App, error) {
+	cfg, err := config.NewConfigBuilder().
+		WithFlagParsing().
+		WithEnvParsing().
+		WithDefaultJWTKey().
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("can't initialize config: %v", err)
+	}
+
+	err = logger.LogInit(cfg.LogLevel)
+	if err != nil {
+		return nil, fmt.Errorf("can't initialize logger: %v", err)
+	}
+
+	scfg := storage.NewStorageConfig(
+		storage.WithDatabaseDsn(cfg.DatabaseDsn),
+		storage.WithFilePath(cfg.StorageFilePath),
+		storage.WithStorageType(cfg.StorageType),
+	)
+	strg, err := storage.Factory(scfg)
+	if err != nil {
+		return nil, fmt.Errorf("can't initialize storage: %v", err)
+	}
+
+	hashService := services.NewHashGen(lengthOfShortURL)
+	shortenerService := services.NewShortener(strg, hashService)
+	batchShortenerService := services.NewBatchShortener(strg, hashService)
+	pingService := services.NewPing(strg)
+
+	shortenHandler := handlers.NewShortenHandler(shortenerService, cfg.ShortBaseAddr)
+	apiShortenHandler := handlers.NewAPIShortenHandler(shortenerService, cfg.ShortBaseAddr)
+	retrieveHandler := handlers.NewRetrieveHandler(shortenerService)
+	shortenBatchHandler := handlers.NewShortenBatchHandler(batchShortenerService, cfg.ShortBaseAddr)
+	retrieveBatchHandler := handlers.NewRetrieveBatchHandler(batchShortenerService, cfg.ShortBaseAddr)
+	pingHandler := handlers.NewPingHandler(pingService)
+
+	r := chi.NewRouter()
+
+	r.Post("/api/shorten/batch", shortenBatchHandler.HandleHTTP)
+	r.Post("/api/shorten", apiShortenHandler.HandleHTTP)
+	r.Get("/api/user/urls", retrieveBatchHandler.HandleHTTP)
+	r.Get("/ping", pingHandler.HandleHTTP)
+	r.Get("/{short}", retrieveHandler.HandleHTTP)
+	r.Post("/", shortenHandler.ServeHTTP)
+
+	return &App{
+		router:  r,
+		storage: strg,
+		cfg:     cfg,
+	}, nil
+}
+
+func (app *App) Run() error {
+	defer app.storage.Close()
+
+	err := http.ListenAndServe(app.cfg.ServerAddr, app.router)
+	if err != nil {
+		return err
+	}
+	return nil
+}
