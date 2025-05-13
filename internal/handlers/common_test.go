b002a1c005be9c7f5847897cd907eea852fab556
Khalikov Ruslan <rycln1@gmail.com>
2025-05-13 17:25:27 +0300

    test: added test for shorten handler

---
+++ internal/handlers/common_test.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"errors"
+
+	"github.com/rycln/shorturl/internal/models"
+)
+
+const (
+	testBaseAddr                     = "test/"
+	testUserID       models.UserID   = "1"
+	testShortURL     models.ShortURL = "abc123"
+	testDeletedShort models.ShortURL = "321cba"
+	testOrigURL      models.OrigURL  = "https://practicum.yandex.ru/"
+)
+
+var (
+	errTest = errors.New("test error")
+
+	testPair = models.URLPair{
+		UID:   testUserID,
+		Short: testShortURL,
+		Orig:  testOrigURL,
+	}
+
+	testDelReq = models.DelURLReq{
+		UID:   testUserID,
+		Short: testDeletedShort,
+	}
+)
---
+++ internal/handlers/shorten_test.go
@@ -0,0 +1,108 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/rycln/shorturl/internal/handlers/mocks"
+	"github.com/rycln/shorturl/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShortenHandler_ServeHTTP(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mShort := mocks.NewMockapiShortenServicer(ctrl)
+	mAuth := mocks.NewMockshortenAuthServicer(ctrl)
+
+	shortenHandler := NewShortenHandler(mShort, mAuth, testBaseAddr)
+
+	t.Run("valid test", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+		mShort.EXPECT().ShortenURL(gomock.Any(), testPair.UID, testPair.Orig).Return(&testPair, nil)
+
+		reqBody := strings.NewReader(string(testPair.Orig))
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		shortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusCreated, res.StatusCode)
+		resBody, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		wantBody := testBaseAddr + "/" + string(testPair.Short)
+		assert.Equal(t, wantBody, string(resBody))
+
+	})
+
+	t.Run("user id error", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(models.UserID(""), errTest)
+
+		reqBody := strings.NewReader(string(testPair.Orig))
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		shortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	})
+
+	t.Run("wrong orig url error", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+
+		reqBody := strings.NewReader("wrong url")
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		shortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("conflict", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+		mErr := mocks.NewMockerrShortenConflict(ctrl)
+		mErr.EXPECT().IsErrConflict().Return(true)
+		mShort.EXPECT().ShortenURL(gomock.Any(), testPair.UID, testPair.Orig).Return(&testPair, mErr)
+
+		reqBody := strings.NewReader(string(testPair.Orig))
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		shortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusConflict, res.StatusCode)
+		resBody, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		wantBody := testBaseAddr + "/" + string(testPair.Short)
+		assert.Equal(t, wantBody, string(resBody))
+	})
+
+	t.Run("some error", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+		mShort.EXPECT().ShortenURL(gomock.Any(), testPair.UID, testPair.Orig).Return(nil, errTest)
+
+		reqBody := strings.NewReader(string(testPair.Orig))
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		shortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	})
+}
