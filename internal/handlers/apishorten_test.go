f6f2fcf77faf88e8c8ef8aca2dcbdb557027cbf7
Khalikov Ruslan <rycln1@gmail.com>
2025-05-13 21:19:00 +0300

    test: updated tests for api shorten handler

--- internal/handlers/apishorten_test.go
+++ internal/handlers/apishorten_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -62,6 +63,90 @@ func TestAPIShortenHandler_HandleHTTP(t *testing.T) {
 		jsonData, err := json.Marshal(&reqOrig)
 		require.NoError(t, err)
 		reqBody := bytes.NewReader(jsonData)
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		apiShortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	})
+
+	t.Run("wrong json", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+
+		reqBody := strings.NewReader("wrong json")
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		apiShortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("wrong url", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+
+		var reqOrig = apiShortenReq{
+			URL: "wrong url",
+		}
+		jsonReq, err := json.Marshal(&reqOrig)
+		require.NoError(t, err)
+		reqBody := bytes.NewReader(jsonReq)
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		apiShortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("conflict", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+		mErr := mocks.NewMockerrAPIShortenConflict(ctrl)
+		mErr.EXPECT().IsErrConflict().Return(true)
+		mShort.EXPECT().ShortenURL(gomock.Any(), testPair.UID, testPair.Orig).Return(&testPair, mErr)
+
+		var reqOrig = apiShortenReq{
+			URL: string(testOrigURL),
+		}
+		jsonReq, err := json.Marshal(&reqOrig)
+		require.NoError(t, err)
+		reqBody := bytes.NewReader(jsonReq)
+		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
+		w := httptest.NewRecorder()
+		apiShortenHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusConflict, res.StatusCode)
+		resBody, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		var resOrig = apiShortenRes{
+			Result: testBaseAddr + "/" + string(testPair.Short),
+		}
+		jsonRes, err := json.Marshal(&resOrig)
+		require.NoError(t, err)
+
+		assert.Equal(t, string(jsonRes)+"\n", string(resBody))
+	})
+
+	t.Run("some shortener service error", func(t *testing.T) {
+		mAuth.EXPECT().GetUserIDFromCtx(gomock.Any()).Return(testPair.UID, nil)
+		mShort.EXPECT().ShortenURL(gomock.Any(), testPair.UID, testPair.Orig).Return(nil, errTest)
+
+		var reqOrig = apiShortenReq{
+			URL: string(testOrigURL),
+		}
+		jsonReq, err := json.Marshal(&reqOrig)
+		require.NoError(t, err)
+		reqBody := bytes.NewReader(jsonReq)
 		req := httptest.NewRequest(http.MethodPost, "/", reqBody)
 		w := httptest.NewRecorder()
 		apiShortenHandler.ServeHTTP(w, req)
