752e84a893ac43412994e582b7db5f2307c91443
Khalikov Ruslan <rycln1@gmail.com>
2025-05-13 22:44:47 +0300

    test: added tests for retrieve handler

---
+++ internal/handlers/retrieve_test.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/rycln/shorturl/internal/handlers/mocks"
+	"github.com/rycln/shorturl/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetrieveHandler_ServeHTTP(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mServ := mocks.NewMockretrieveServicer(ctrl)
+
+	retrieveHandler := NewRetrieveHandler(mServ)
+
+	t.Run("valid test", func(t *testing.T) {
+		mServ.EXPECT().GetShortURLFromCtx(gomock.Any()).Return(testShortURL, nil)
+		mServ.EXPECT().GetOrigURLByShort(gomock.Any(), testShortURL).Return(testOrigURL, nil)
+
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		w := httptest.NewRecorder()
+		retrieveHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusTemporaryRedirect, res.StatusCode)
+		assert.Equal(t, res.Header.Get("Location"), string(testOrigURL))
+	})
+
+	t.Run("short url error", func(t *testing.T) {
+		mServ.EXPECT().GetShortURLFromCtx(gomock.Any()).Return(models.ShortURL(""), errTest)
+
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		w := httptest.NewRecorder()
+		retrieveHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	})
+
+	t.Run("url was deleted", func(t *testing.T) {
+		mServ.EXPECT().GetShortURLFromCtx(gomock.Any()).Return(testShortURL, nil)
+		mErr := mocks.NewMockerrRetrieveDeletedURL(ctrl)
+		mErr.EXPECT().IsErrDeletedURL().Return(true)
+		mServ.EXPECT().GetOrigURLByShort(gomock.Any(), testShortURL).Return(models.OrigURL(""), mErr)
+
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		w := httptest.NewRecorder()
+		retrieveHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusGone, res.StatusCode)
+	})
+
+	t.Run("some service error", func(t *testing.T) {
+		mServ.EXPECT().GetShortURLFromCtx(gomock.Any()).Return(testShortURL, nil)
+		mServ.EXPECT().GetOrigURLByShort(gomock.Any(), testShortURL).Return(models.OrigURL(""), errTest)
+
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		w := httptest.NewRecorder()
+		retrieveHandler.ServeHTTP(w, req)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	})
+}
