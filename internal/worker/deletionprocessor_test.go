777d56f66f79f6370a79d986d40e8d0d0c8c5445
Khalikov Ruslan <rycln1@gmail.com>
2025-05-14 10:10:02 +0300

    test: updated tests for delete worker

--- internal/worker/deletionprocessor_test.go
+++ internal/worker/deletionprocessor_test.go
@@ -1,78 +1,94 @@
 package worker
 
 import (
-	"context"
-	"fmt"
+	"errors"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/rycln/shorturl/internal/models"
-	"github.com/stretchr/testify/assert"
+	"github.com/rycln/shorturl/internal/worker/mocks"
 )
 
 const (
-	testUserID = models.UserID("1")
+	testUserID  = models.UserID("1")
+	testTicker  = time.Duration(10) * time.Millisecond
+	testTimeout = time.Duration(10) * time.Millisecond
 )
 
-func TestDeletionProcessor_AddURLsIntoDeletionQueue(t *testing.T) {
-	t.Run("valid test", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+var errTest = errors.New("test error")
 
-		p := &DeletionProcessor{
-			ctx:      ctx,
-			cancel:   cancel,
-			delChans: make(chan chan *models.DelURLReq, 10),
-		}
+func TestDeletionProcessor_Run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("valid test", func(t *testing.T) {
+		mServ := mocks.NewMockbatchDeleteServicer(ctrl)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		mServ.EXPECT().DeleteURLsBatch(gomock.Any(), gomock.Any()).Return(nil).Times(1).Do(func(_, _ interface{}) {
+			wg.Done()
+		})
+
+		p := NewDeletionProcessor(mServ)
+		defer p.Shutdown()
 
 		urls := []models.ShortURL{"url1", "url2", "url3"}
 
+		p.Run(testTicker, testTimeout)
 		p.AddURLsIntoDeletionQueue(testUserID, urls)
 
-		delCh := <-p.delChans
-
-		var received []*models.DelURLReq
-		for req := range delCh {
-			received = append(received, req)
-		}
-
-		assert.Equal(t, len(urls), len(received))
-
-		for i, req := range received {
-			assert.Equal(t, testUserID, req.UID)
-			assert.Equal(t, urls[i], req.Short)
-		}
-	})
-
-	t.Run("cancellation", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		p := &DeletionProcessor{
-			ctx:      ctx,
-			cancel:   cancel,
-			delChans: make(chan chan *models.DelURLReq, 10),
-		}
-
-		urls := make([]models.ShortURL, 1000)
-		for i := range urls {
-			urls[i] = models.ShortURL(fmt.Sprintf("url%d", i))
-		}
+		var done = make(chan struct{})
 
 		go func() {
-			time.Sleep(10 * time.Microsecond)
-			p.cancel()
+			wg.Wait()
+			close(done)
 		}()
 
+		<-done
+	})
+
+	t.Run("serv error", func(t *testing.T) {
+		mServ := mocks.NewMockbatchDeleteServicer(ctrl)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		mServ.EXPECT().DeleteURLsBatch(gomock.Any(), gomock.Any()).Return(errTest).Times(1).Do(func(_, _ interface{}) {
+			wg.Done()
+		})
+
+		p := NewDeletionProcessor(mServ)
+		defer p.Shutdown()
+
+		urls := []models.ShortURL{"url1", "url2", "url3"}
+
+		p.Run(testTicker, testTimeout)
 		p.AddURLsIntoDeletionQueue(testUserID, urls)
-		delCh := <-p.delChans
 
-		count := 0
-		for range delCh {
-			count++
-		}
+		var done = make(chan struct{})
 
-		assert.NotEqual(t, 0, count)
-		assert.NotEqual(t, len(urls), count, "cancel did not work")
+		go func() {
+			wg.Wait()
+			close(done)
+		}()
+
+		<-done
+	})
+
+	t.Run("shutdown", func(t *testing.T) {
+		mServ := mocks.NewMockbatchDeleteServicer(ctrl)
+
+		p := NewDeletionProcessor(mServ)
+
+		urls := []models.ShortURL{"url1", "url2", "url3"}
+
+		p.Run(testTicker, testTimeout)
+		p.AddURLsIntoDeletionQueue(testUserID, urls)
+
+		p.Shutdown()
 	})
 }
