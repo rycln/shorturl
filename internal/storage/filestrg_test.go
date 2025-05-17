1a247d75c5ed97f10e0babe66aaa040060789a2c
Khalikov Ruslan <rycln1@gmail.com>
2025-05-17 12:53:14 +0300

    test: added benchmarks for file storage

--- internal/storage/filestrg_test.go
+++ internal/storage/filestrg_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"testing"
 
@@ -51,6 +52,85 @@ func TestFileStorage_AddURLPair(t *testing.T) {
 	})
 }
 
+func BenchmarkFileStorage_AddURLPair(b *testing.B) {
+	b.Run("add unique pair", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.AddURLPair(context.Background(), &testPair)
+		}
+	})
+
+	b.Run("add 100 unique pairs", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 100
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			for j := range len {
+				storage.AddURLPair(context.Background(), &pairs[j])
+			}
+		}
+	})
+
+	b.Run("add 1000 unique pairs", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 1000
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			for j := range len {
+				storage.AddURLPair(context.Background(), &pairs[j])
+			}
+		}
+	})
+
+	b.Run("add duplicate pair", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		storage.AddURLPair(context.Background(), &testPair)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.AddURLPair(context.Background(), &testPair)
+		}
+	})
+}
+
 func TestFileStorage_GetURLPairByShort(t *testing.T) {
 	strg, err := NewFileStorage(testFileName)
 	require.NoError(t, err)
@@ -95,6 +175,22 @@ func TestFileStorage_GetURLPairByShort(t *testing.T) {
 	})
 }
 
+func BenchmarkFileStorage_GetURLPairByShort(b *testing.B) {
+	b.Run("get pair", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		_ = storage.AddURLPair(context.Background(), &testPair)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.GetURLPairByShort(context.Background(), testShortURL)
+		}
+	})
+}
+
 func TestFileStorage_AddBatchURLPairs(t *testing.T) {
 	strg, err := NewFileStorage(testFileName)
 	require.NoError(t, err)
@@ -125,6 +221,67 @@ func TestFileStorage_AddBatchURLPairs(t *testing.T) {
 
 		err := strg.AddBatchURLPairs(ctx, pairs)
 		assert.Error(t, err)
+	})
+}
+
+func BenchmarkFileStorage_AddBatchURLPairs(b *testing.B) {
+	b.Run("add pair", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		pair := make([]models.URLPair, 1)
+		pair[0] = testPair
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.AddBatchURLPairs(context.Background(), pair)
+		}
+	})
+
+	b.Run("add 100 pairs", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 100
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.AddBatchURLPairs(context.Background(), pairs)
+		}
+	})
+
+	b.Run("add 1000 pairs", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 1000
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.AddBatchURLPairs(context.Background(), pairs)
+		}
 	})
 }
 
@@ -179,6 +336,107 @@ func TestFileStorage_GetURLPairBatchByUserID(t *testing.T) {
 	})
 }
 
+func BenchmarkFileStorage_GetURLPairBatchByUserID(b *testing.B) {
+	b.Run("get url", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		testPairs := []models.URLPair{
+			testPair,
+		}
+
+		file, err := os.OpenFile(storage.strgFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		require.NoError(b, err)
+		defer file.Close()
+
+		enc := json.NewEncoder(file)
+		require.NoError(b, err)
+
+		for _, pair := range testPairs {
+			err = enc.Encode(&pair)
+			require.NoError(b, err)
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.GetURLPairBatchByUserID(context.Background(), testUserID)
+		}
+	})
+
+	b.Run("get 100 urls", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 100
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		file, err := os.OpenFile(storage.strgFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		require.NoError(b, err)
+		defer file.Close()
+
+		enc := json.NewEncoder(file)
+		require.NoError(b, err)
+
+		for _, pair := range pairs {
+			err = enc.Encode(&pair)
+			require.NoError(b, err)
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.GetURLPairBatchByUserID(context.Background(), testUserID)
+		}
+	})
+
+	b.Run("get 1000 urls", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 1000
+		pairs := make([]models.URLPair, len)
+		for i := range len {
+			pairs[i] = models.URLPair{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Orig:  models.OrigURL(fmt.Sprintf("https://site.com/page%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		file, err := os.OpenFile(storage.strgFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		require.NoError(b, err)
+		defer file.Close()
+
+		enc := json.NewEncoder(file)
+		require.NoError(b, err)
+
+		for _, pair := range pairs {
+			err = enc.Encode(&pair)
+			require.NoError(b, err)
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.GetURLPairBatchByUserID(context.Background(), testUserID)
+		}
+	})
+}
+
 func TestFileStorage_DeleteRequestedURLs(t *testing.T) {
 	strg, err := NewFileStorage(testFileName)
 	require.NoError(t, err)
@@ -206,6 +464,69 @@ func TestFileStorage_DeleteRequestedURLs(t *testing.T) {
 	})
 }
 
+func BenchmarkFileStorage_DeleteRequestedURLs(b *testing.B) {
+	b.Run("delete request", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		delURLs := []*models.DelURLReq{
+			&testDelReq,
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.DeleteRequestedURLs(context.Background(), delURLs)
+		}
+	})
+
+	b.Run("100 delete requests", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 100
+		delURLs := make([]*models.DelURLReq, len)
+		for i := range len {
+			delURLs[i] = &models.DelURLReq{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.DeleteRequestedURLs(context.Background(), delURLs)
+		}
+	})
+
+	b.Run("1000 delete requests", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		len := 1000
+		delURLs := make([]*models.DelURLReq, len)
+		for i := range len {
+			delURLs[i] = &models.DelURLReq{
+				UID:   models.UserID(fmt.Sprintf("user-%d", i)),
+				Short: models.ShortURL(fmt.Sprintf("hash-%d", i)),
+			}
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.DeleteRequestedURLs(context.Background(), delURLs)
+		}
+	})
+}
+
 func TestFileStorage_Ping(t *testing.T) {
 	strg, err := NewFileStorage(testFileName)
 	require.NoError(t, err)
@@ -215,5 +536,20 @@ func TestFileStorage_Ping(t *testing.T) {
 	t.Run("valid test", func(t *testing.T) {
 		err := strg.Ping(context.Background())
 		assert.NoError(t, err)
+	})
+}
+
+func BenchmarkFileStorage_Ping(b *testing.B) {
+	b.Run("ping", func(b *testing.B) {
+		storage, err := NewFileStorage(testFileName)
+		require.NoError(b, err)
+		defer os.Remove(storage.strgFileName)
+		defer os.Remove(storage.delFileName)
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			storage.Ping(context.Background())
+		}
 	})
 }
